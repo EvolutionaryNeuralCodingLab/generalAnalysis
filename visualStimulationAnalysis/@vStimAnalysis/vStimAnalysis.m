0283d843a93835d39bbe427e46b97c1d1e26afcf
Mark Shein-Idelson <shein.mark@gmail.com>
2025-07-02 13:35:37 +0300

    Building vStim + minor bug fix in NPAP

--- visualStimulationAnalysis/@vStimAnalysis/vStimAnalysis.m
+++ visualStimulationAnalysis/@vStimAnalysis/vStimAnalysis.m
@@ -1,34 +1,41 @@
 classdef vStimAnalysis
 
     properties
-        vStimClass(1,1) string {mustBeMember(vStimClass,...
-            {"rectGrid","linearlyMovingBall","linearlyMovingBarWidth","linearlyMovingDotsWrapped","rectNoiseGrid"})} = "";
+        vStimClass(1,1) string {mustBeMember(vStimClass,{'','rectGrid','linearlyMovingBall','linearlyMovingBarWidth','linearlyMovingDotsWrapped','rectNoiseGrid'})}='';
+        diodeUpCross
+        diodeDownCross
     end
 
     methods (Hidden)
 
         %class constructor
-        function obj=vStimAnalysis(vStimClassValue) 
-            obj.vStimClass=vStimClassValue;
+        function obj=vStimAnalysis(vStimClassValue)
+            if isempty(vStimClassValue)
+                disp('No visual class was entered, please define the vStimClass property.');
+            else
+                obj.vStimClass=vStimClassValue;
+            end
         end
     end
 
     methods
 
-        function [upCross,downCross]=getDiodeTriggers(dataObj,analogDataCh,extractionMethod)
-            arguments
-                dataObj 
+        function [upCross,downCross]=getDiodeTriggers(obj,dataObj,analogDataCh,extractionMethod)
+            arguments (Input)
+                obj
+                dataObj
+                analogDataCh
                 extractionMethod (1,:) string {mustBeMember(extractionMethod,...
-                    {"diodeThreshold","digitalTriggerDiode"})} = "diodeThreshold";
+                    {'diodeThreshold','digitalTriggerDiode'})} = 'diodeThreshold';
             end
 
             switch extractionMethod
                 case "diodeThreshold"
-                    [A,t_ms]=dataObj.getAnalogData(analogDataCh,0,analogDataObj.recordingDuration_ms);
+                    [A,t_ms]=dataObj.getAnalogData(analogDataCh,0,dataObj.recordingDuration_ms); %extract diode data for entire recording 
 
                     Th=mean(A(1:100:end));
-                    upCross=t_ms(A(1:end-1)<Th & A(2:end)>=Th);
-                    downCross=t_ms(A(1:end-1)>Th & A(2:end)<=Th);
+                    obj.diodeUpCross=t_ms(A(1:end-1)<Th & A(2:end)>=Th);
+                    obj.diodeDownCross=t_ms(A(1:end-1)>Th & A(2:end)<=Th);
 
                 case "digitalTriggerDiode"
 
