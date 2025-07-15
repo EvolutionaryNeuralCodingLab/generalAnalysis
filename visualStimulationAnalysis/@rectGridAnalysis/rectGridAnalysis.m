447fe1dc89449dd36e3002ea306b6bf25b304379
Mark Shein-Idelson <shein.mark@gmail.com>
2025-07-15 21:48:01 +0300

    LFP methods

--- visualStimulationAnalysis/@rectGridAnalysis/rectGridAnalysis.m
+++ visualStimulationAnalysis/@rectGridAnalysis/rectGridAnalysis.m
@@ -23,22 +23,62 @@ classdef rectGridAnalysis < VStimAnalysis
                 obj
                 params.overwrite logical = false
                 params.analysisTime = datetime('now')
+                params.inputParams = false
+                params.normTrace = true
             end
-
-
-            LFP=vs.getStimLFP;
-            vStimGrid=nan(vs.VST.rectGridSize,vs.VST.rectGridSize);
-            vStimGrid(sub2ind([vs.VST.rectGridSize',vs.VST.rectGridSize],vs.VST.pos2Y,vs.VST.pos2X))=1:(vs.VST.rectGridSize^2);
+            if params.inputParams,disp(params),return,end
+            
+            LFP=obj.getStimLFP;
+            vStimGrid=nan(obj.VST.rectGridSize,obj.VST.rectGridSize);
+            vStimGrid(sub2ind([obj.VST.rectGridSize',obj.VST.rectGridSize],obj.VST.pos2Y,obj.VST.pos2X))=1:(obj.VST.rectGridSize^2);
             vStimGrid=flipud(vStimGrid);
 
-            [~,pOrdered]=sort(vs.VST.pos);
+            [~,pOrdered]=sort(obj.VST.pos);
             [nElecs,nTrials,nBins]=size(LFP.LFP_on);
-    
+            mFLP_on=squeeze(mean(reshape(LFP.LFP_on(:,pOrdered,:),[nElecs,obj.VST.trialsPerCategory,nTrials/obj.VST.trialsPerCategory,nBins]),2));
+            mFLP_off=squeeze(mean(reshape(LFP.LFP_off(:,pOrdered,:),[nElecs,obj.VST.trialsPerCategory,nTrials/obj.VST.trialsPerCategory,nBins]),2));
             
-            tmp=reshape(LFP.LFP_on(:,pOrdered,:),[nElecs,vs.VST.trialsPerCategory,nTrials/vs.VST.trialsPerCategory,nBins]);
+            if params.normTrace
+                mFLP_on=bsxfun(@minus,mFLP_on,mean(mFLP_on,3));
+                mFLP_off=bsxfun(@minus,mFLP_off,mean(mFLP_off,3));
+            end
 
-            %tmp=reshape(permute(LFP.LFP_on(:,pOrdered,:),[3,1,2]),[nBins,nElecs,vs.VST.trialsPerCategory,nTrials/vs.VST.trialsPerCategory]);
+            stdOn=std(mFLP_on(:));
+            stdOff=std(mFLP_off(:));
 
+            f1=figure('Name','On responses vs position');hL=tiledlayout(obj.VST.rectGridSize,obj.VST.rectGridSize,'TileSpacing','tight','Padding','tight','Visible','off');
+            for i=1:obj.VST.rectGridSize^2
+                nexttile;
+                imagesc(squeeze(mFLP_on(:,i,:)));
+                set(gca,'XTick',[],'YTick',[],'CLim',[-stdOn*2 stdOn*2]);
+            end
+            hL.Visible='on';
+
+            f2=figure('Name','Off responses vs position');hL=tiledlayout(obj.VST.rectGridSize,obj.VST.rectGridSize,'TileSpacing','tight','Padding','tight','Visible','off');
+            for i=1:obj.VST.rectGridSize^2
+                nexttile;
+                imagesc(squeeze(mFLP_off(:,i,:)));
+                set(gca,'XTick',[],'YTick',[],'CLim',[-stdOff*2 stdOff*2]);
+            end
+            hL.Visible='on';
+
+            f3=figure('Name','On averaged across positions');
+            imagesc((1:nBins)/LFP.samplingFreqLFP*1000,1:nElecs,squeeze(mean(mFLP_on,2)));
+            ylabel('Ch #');
+            xlabel('Time [ms]');
+
+            f4=figure('Name','Off averaged across positions');
+            imagesc((1:nBins)/LFP.samplingFreqLFP*1000,1:nElecs,squeeze(mean(mFLP_off,2)));
+            ylabel('Ch #');
+            xlabel('Time [ms]');
+
+            f4=figure('Name','Off averaged across positions');
+
+
+            [hPlot]=activityMultiTracePhysicalSpacePlot(h,(1:32)',squeeze(M_Int),vStimGrid,'DrawElectrodeNumbers',0,'traceColor',[0.8 0.2 0.2]);
+            [hScaleBar]=addScaleBar(h,'scaleFac',1.4);
+            activityTracePhysicalSpacePlot(h,IntanData.channelNumbers(:),squeeze(mean(M_Intan(:,pOrdered(1),:),2)),IntanData.chLayoutNumbers,'traceColor',[0.8 0.2 0.2],'DrawElectrodeNumbers',0);
+            
             subplot(1,2,1);imagesc(squeeze(mean(tmp(:,:,2,:),2)));
             subplot(1,2,2);imagesc(squeeze(mean(LFP.LFP_on(:,pOrdered(16:30),:),2)));
 
@@ -50,12 +90,9 @@ classdef rectGridAnalysis < VStimAnalysis
             mean(LFP.LFP_on)
 
 
-            f=figure;h=axes;
-            [hPlot]=activityMultiTracePhysicalSpacePlot(h,(1:32)',squeeze(M_Int),vStimGrid,'DrawElectrodeNumbers',0,'traceColor',[0.8 0.2 0.2]);
-            [hScaleBar]=addScaleBar(h,'scaleFac',1.4);
+
 
                 f=figure;h=axes;
-                activityTracePhysicalSpacePlot(h,IntanData.channelNumbers(:),squeeze(mean(M_Intan(:,pOrdered(1),:),2)),IntanData.chLayoutNumbers,'traceColor',[0.8 0.2 0.2],'DrawElectrodeNumbers',0);
             %}
         end
     end
